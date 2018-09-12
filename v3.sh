12f83b18b42ca7de4fd0580acefd27cad8225599
Gurpalw <gurpal.walia@viooh.com>
2018-09-12 17:01:59 +0100

    Add filename flag

--- v3.sh
+++ v3.sh
@@ -10,7 +10,7 @@ unset dbusername
 usage()
 {
   echo "Missing or incorrect flags. All flags mandatory."
-  echo "Usage: backupdb -h host -d databasename -u username"
+  echo "Usage: backupdb -h host -d databasename -u username -f filename"
   exit 1
 }
 
@@ -24,21 +24,21 @@ rm $filename.sql.gz
 exit 0
 }
 
-while getopts h:d:u: options; do
+while getopts h:d:u:f: options; do
   case $options in
     h) dbhost=$OPTARG;;
     d) dbname=$OPTARG;;
     u) dbusername=$OPTARG;;
+    f) filename=$OPTARG;;
     esac
 done
 
-if [ -z $dbhost ] || [ -z $dbname ] || [ -z $dbusername ]; then
+if [ -z $dbhost ] || [ -z $dbname ] || [ -z $dbusername ] || [ -z $filename ]; then
   usage
 fi
 
 read -s -p "DB Password: " dbpassword
 echo
-read -p "Filename for dump: " filename
 echo "Attempting to dump $dbname from $dbhost to $filename.sql.gz"
 echo "mysqldump --verbose -h $dbhost --max_allowed_packet=1G --extended-insert --single-transaction --add-drop-database --opt $dbname --user=$dbusername --password=xxxxxx | gzip -1 > $filename.sql.gz"
 set -o pipefail
